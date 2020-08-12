4e2b7da5bdc755947be976d41ac57ee05867a53a
macas5 <mkalinauskas1993@gmail.com>
2020-08-12 09:06:48 -0700

    Updates

---
+++ precache-manifest.05f718dbd21d3a0f33e2deb6301a15cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5df723ad2d8995b2a7a9de1fb932a0",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "cdb2d97b180d7b303dfd",
+    "url": "/smart-brain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a10b79d47daff5c3c047",
+    "url": "/smart-brain/static/css/main.37206f77.chunk.css"
+  },
+  {
+    "revision": "cdb2d97b180d7b303dfd",
+    "url": "/smart-brain/static/js/2.9a91bbaa.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/smart-brain/static/js/2.9a91bbaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a10b79d47daff5c3c047",
+    "url": "/smart-brain/static/js/main.86b1b8c8.chunk.js"
+  },
+  {
+    "revision": "5f284a9b7132a669a655",
+    "url": "/smart-brain/static/js/runtime-main.43daafc1.js"
+  }
+]);
