32bcb868b922ce7d10e2b1a42991d7449abddb18
macas5 <mkalinauskas1993@gmail.com>
2020-08-17 12:33:31 -0700

    fixed ImageLinkForm responsiveness

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -9,9 +9,9 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit, onEnterPress}) => {
       </p>
       <div className='center'>
         <div className='center form pa4 br3 shadow-5 flex flex-wrap justify-center'>
-          <input className='f4 pa2 w-70 center' type='text'
+          <input className='f4 pa2 w-auto center' type='text'
            onKeyPress={onEnterPress} onChange={onInputChange} />
-          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+          <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple minw120'
           onClick={onButtonSubmit}>  Detect</button>
         </div>
       </div>
